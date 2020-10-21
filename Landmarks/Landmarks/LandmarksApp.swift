7610d87cfcc0e91fdec00b5c1cdd888395dddf70
Stat.So <stat.so@insiar.com>
2020-10-21 16:41:39 +0900

    - Initial commit

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Stat.So on 2020/10/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
