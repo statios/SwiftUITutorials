b9ea9171ab9b5b72e0b92f8ebfac7b83a72835c5
Stat.So <statios@kakao.com>
2020-10-24 17:37:42 +0900

    - Composing Complex Interfaces

---
+++ ComposingComplexInterfaces/StartingPoint/Landmarks/Landmarks/Home.swift
@@ -0,0 +1,62 @@
+//
+//  Home.swift
+//  Landmarks
+//
+//  Created by Stat.So on 2020/10/24.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+  var categories: [String: [Landmark]] {
+    //landmarData를 landmarData.category.rawValue 기준으로 grouping
+    Dictionary(grouping: landmarkData) { $0.category.rawValue }
+  }
+  var featured: [Landmark] {
+    landmarkData.filter { $0.isFeatured }
+  }
+  @State var showingProfile = false
+  var profileButton: some View {
+    Button(action: { self.showingProfile.toggle() }, label: {
+      Image(systemName: "person.crop.circle")
+        .imageScale(.large)
+        .accessibility(label: Text("User Profile"))
+        .padding()
+    })
+  }
+  var body: some View {
+    NavigationView {
+      List {
+        FeaturedLandmarks(landmarks: featured)
+          .scaledToFill()
+          .frame(height: 200)
+          .clipped()
+          .listRowInsets(EdgeInsets())
+        ForEach(categories.keys.sorted(), id: \.self) { key in
+          CategoryRow(categoryName: key, items: self.categories[key]!) }
+          .listRowInsets(EdgeInsets())
+        NavigationLink(destination: LandmarkList(),
+                       label: { Text("See All") })
+      }
+      .navigationBarTitle(Text("Featured"))
+      .navigationBarItems(trailing: profileButton)
+      .sheet(isPresented: $showingProfile) {
+        Text("User Profile")
+      }
+    }
+  }
+}
+
+struct FeaturedLandmarks: View {
+  var landmarks: [Landmark]
+  var body: some View {
+    landmarks[0].image.resizable()
+  }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+  static var previews: some View {
+    CategoryHome()
+  }
+}
