f74d62dd14180fd585343e736c075c65dfeab96d
Stat.So <statios@kakao.com>
2020-11-24 10:58:51 +0900

    update resources

---
+++ InterfacingWithUIKit/StartingPoint/Landmarks/Landmarks/PageView.swift
@@ -0,0 +1,27 @@
+//
+//  PageView.swift
+//  Landmarks
+//
+//  Created by Stat.So on 2020/10/24.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+  var viewControllers: [UIHostingController<Page>]
+  
+  init(_ views: [Page]) {
+    self.viewControllers = views.map { UIHostingController(rootView: $0)}
+  }
+  var body: some View {
+    PageViewController(controllers: viewControllers)
+  }
+}
+
+struct PageView_Previews: PreviewProvider {
+  static var previews: some View {
+    PageView(features.map { FeatureCard(landmark: $0) })
+      .aspectRatio(3/2, contentMode: .fit)
+  }
+}
